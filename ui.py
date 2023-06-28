8ce9f21437854059f5e4db2bd5d2ede8308d12db
jordansbenjamin <14327@coderacademy.edu.au>
2023-06-28 22:33:30 +1000

    Added initial UI canvas setup

--- ui.py
+++ ui.py
@@ -1,2 +1,16 @@
-THEME_COLOR = "#375362"
+from tkinter import *
 
+THEME_COLOR = "#375362"
+FONT = ('Arial', 20, 'italic')
+
+class QuizInterface:
+    def __init__(self) -> None:
+        self.window = Tk()
+        self.window.title("Quizzler")
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+
+        self.canvas = Canvas(height=250, width=300, bg='white', highlightthickness=0)
+        self.canvas.create_text(150,125,text="Test", font=FONT, fill='black')
+        self.canvas.grid(column=0, row=0)
+
+        self.window.mainloop()
